b99f9ad3952daa588ec1e71798d44e7b710213d6
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-04-13 16:27:37 +0200

    Add ex4 allocation

--- allocation.c
+++ allocation.c
@@ -6,6 +6,41 @@
     #define M_PI 3.14159265358979323846
 #endif*/
 
+void ex4()
+{
+    printf("Ex 4 allocation\n");
+
+    int size = 7;
+    int* tab = (int*)malloc(sizeof(int) * size);
+    if( tab == NULL )
+        exit(EXIT_FAILURE);
+
+    for(int i = 0; i<20; i++){
+        tab[i] = i;
+        
+        if( i < size )
+            tab[i] = i;
+        else{
+            int* tmp = (int*)malloc(sizeof(int) * size * 2);
+
+            if( tmp == NULL ){
+                free(tab);
+                exit(EXIT_FAILURE);
+            }
+
+            memcpy(tmp, tab, sizeof(int)*size);
+            printf("La nouvelle taille du tableau vaut : %d\n", size );
+
+            size *= 2;
+
+            free(tab); // libèrer la première zone
+            tab = tmp; // redirige tab sur la nouvelle zone
+        }
+    }
+
+    free(tab);
+}
+
 void allocation()
 {
     printf("\033[1;32mAllocation de mémoire\n\033[0m");
--- labo.c
+++ labo.c
@@ -3,34 +3,50 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-typedef struct {
+#define NUMBER_VAL 12
+
+typedef struct
+{
     int x;
     int y;
 } Point;
 
-void func(Point* p){
+void func(Point* p)
+{
     p->x = 0;
     p->y = 0;
 }
 
-void labo(){
+void labo()
+{
     printf("Exemple labo\n");
-    // Déclaration de la variable i
-    int i;
+    // Déclaration de la variable i // commentaire inutile
+    // int i; réduit scope
 
     int a;
     double frequency = 2.3;
 
+    int VarVar;
+    int varVar;
+    int var_var;
+    int addIpHtml;
+
     printf("Veuillez entrer la valeur de a \n>");
-    scanf("%d", &a);
+    
 
-    double result = a * 2.3+4;
+    printf("Valeur %d", a);
 
+    double result = a * 2.3 + 4;
 
-    Point p;
-    p.x = 12;
+    {
+        Point p;
+        p.x = 12;
+    }
 
-    for(i=4; i>0; i--){
+    printf("Ma valeur %d", a);
+
+    for (int i = 4; i > 0; i--)
+    {
         printf("Res 1 : %d\n", a);
         printf("Res 2 : %.2lf\n", frequency);
     }
