efa371f9fff988052d2657f75b39206a17e75a60
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-06-03 10:04:28 +0200

    Add fifo

--- liste.c
+++ liste.c
@@ -106,6 +106,146 @@ void test_pile() {
 }
 
 
+// File
+typedef struct
+{
+    int id;
+    double value;
+} DataFile;
+
+struct ElemFile
+{
+    DataFile val;
+    struct ElemFile* next;
+};
+typedef struct ElemFile ElemFile;
+// ==
+/*
+typedef struct ElemFile
+{
+    Data val;
+    struct ElemFile* next;
+} ElemFile;*/
+
+typedef struct
+{
+    ElemFile* head;
+    ElemFile* queue;
+} File;
+
+bool inserer_file(File* f, DataFile* v)
+{
+    if (f == NULL)
+    {
+        return false;
+    }
+
+    ElemFile* e = malloc(sizeof(ElemFile));
+    if (e == NULL)
+    {
+        return false;
+    }
+
+    e->val = *v;
+    e->next = NULL;
+    
+    if( f->head == NULL ){
+        f->head = e;
+    }
+    else{
+        f->queue->next = e;
+    }
+
+    f->queue = e;
+
+    return true;
+}
+
+bool retirer_file(File* f, DataFile* v)
+{
+    if (f == NULL || f->head == NULL)
+    {
+        return false;
+    }
+
+    ElemFile* cur = f->head;
+    *v = cur->val;
+
+    f->head = cur->next;
+
+    if( f->head == NULL ){
+        f->queue = NULL;
+    }
+
+    free(cur);
+
+    return true;
+}
+
+bool find_file(File* f, int id, DataFile* v){
+
+    if(f->head == NULL){
+        return false;
+    }
+
+    ElemFile* current = f->head;
+    do
+    {
+        if (current->val.id == id)
+        {
+            *v = current->val;
+            return true;
+        }
+        current = current->next;
+    } while (current != NULL);
+
+    return false;
+}
+
+int count_file(File* f){
+    ElemFile* current = f->head;
+    int cpt = 0;
+    while ( current != NULL )
+    {
+        cpt++;
+        current = current->next;
+    }
+    return cpt;
+}
+
+void test_file()
+{
+    printf("\nTest file\n");
+    File f = {0};
+    DataFile d = {.id=12, .value=5.6};
+
+    DataFile dd = (DataFile){4, 4.1};
+
+    inserer_file(&f, &d);
+    inserer_file(&f, &dd);
+    inserer_file(&f, &((DataFile){4, 4.1}));
+    inserer_file(&f, &((DataFile){2, 2.6}));
+    inserer_file(&f, &((DataFile){3, 3.2}));
+
+    DataFile v;
+
+    if(find_file(&f, 2, &v)){
+        printf("Elem find : %d  / %.2lf\n", v.id, v.value);
+    }
+
+    while (retirer_file(&f, &v))
+    {
+        printf("Elem : %d  / %.2lf\n", v.id, v.value);
+    }
+}
+
+/*
+- FIFO, first in, first out
+- inserer
+- retirer
+- afficher la liste des éléments
+*/
+
 
 void liste(){
     printf("\033[1;34mExemple de liste\n\033[0m");
@@ -127,4 +267,6 @@ void liste(){
     printf("La valeur = %d\n", val);
 
     test_pile();
+
+    test_file();
 }
